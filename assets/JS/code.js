d0b90ba7fab2fb225945e102202212f6b624090c
Yfopla <ekblecker@gmail.com>
2020-03-22 15:34:09 -0400

    Fixed: Jquery now working. Added: First ajax call to console log

--- assets/JS/code.js
+++ assets/JS/code.js
@@ -1,2 +1,73 @@
-const api = "74dbb7cafa12a6c802c05abee24b4cd2"
+$(document).ready(function () {
 
+const apiKey = "&appid=74dbb7cafa12a6c802c05abee24b4cd2"
+const pastCity = [];
+
+// Gets past searches from local storage, if any
+if( localStorage.getItem("citySearch")){
+    searchedCities = JSON.parse(localStorage.getItem("citySearch"));
+  }
+
+// Ajax call to get the current conditions
+
+$('#getWeather').on("click", function(){
+    let search = $("#citySearch").val()
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + search + apiKey
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(data){
+        console.log(data)
+
+    })
+    
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//DO NOT DELETE
+})
